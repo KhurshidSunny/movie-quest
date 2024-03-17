759f81dfc86eb1498f025e0a860b830aa6185b67
Khurshid Khan <khurshidsunny45@gmail.com>
2024-03-17 15:31:29 +0500

    change app title to movie when clicked and cleanup fetch request

--- src/App.js
+++ src/App.js
@@ -30,12 +30,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s={${query}}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s={${query}}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -45,8 +48,11 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -58,6 +64,9 @@ export default function App() {
         }
       }
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
@@ -237,7 +246,6 @@ function MovieDetail({ selectedId, onCloseMovie, onAddMovie, watched }) {
           );
           const data = await res.json();
 
-          console.log(data);
           setMovie(data);
         } catch (err) {
           console.log(err.message);
@@ -250,6 +258,18 @@ function MovieDetail({ selectedId, onCloseMovie, onAddMovie, watched }) {
     },
     [selectedId]
   );
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+      return function () {
+        document.title = "Movie Quest";
+      };
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
