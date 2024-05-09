da6cef17e2b63d1ec8103dc78123a02a18c259b6
Khurshid Khan <khurshidsunny45@gmail.com>
2024-05-10 01:10:45 +0500

    fixed another bug

--- src/useMovies.js
+++ src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s={${query}}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
